309bb92d823bf369fbf6e57dd9cc6bc21e8f41b8
aletorres73 <torresalejandro7392@gmail.com>
2026-05-05 17:32:49 -0300

    feat: implement lazy loading for dashboard and order screens to improve performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SalesDashboard from './features/ui/dashboard/screens/SalesDashboard'; // El nuevo componente
-import OrderScreen from "./features/ui/orders/screens/OrdersScreen"
-import CashFlowDashboard from './features/ui/dashboard/screens/CashflowDashboard';
+import { lazy, Suspense } from 'react';
+
+const SalesDashboard = lazy(() => import('./features/ui/dashboard/screens/SalesDashboard'));
+const OrderScreen = lazy(() => import('./features/ui/orders/screens/OrdersScreen'));
+const CashFlowDashboard = lazy(() => import('./features/ui/dashboard/screens/CashflowDashboard'));
 
 
 export default function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<OrderScreen />} />
-        <Route path="/reports" element={<SalesDashboard />} />
-        <Route path="/cashflow" element={<CashFlowDashboard />} />
-      </Routes>
+      <Suspense fallback={<div style={{ color: '#54C4F0' }}>Cargando Feli App...</div>}>
+        <Routes>
+          <Route path="/" element={<OrderScreen />} />
+          <Route path="/reports" element={<SalesDashboard />} />
+          <Route path="/cashflow" element={<CashFlowDashboard />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
